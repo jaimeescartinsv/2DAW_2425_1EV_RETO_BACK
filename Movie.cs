05a6c93b764b3b21734817dcc2c4e31519ca50dc
jaimeescartinsv <a27421@svalero.com>
2024-11-13 19:57:20 +0100

    commit inicial

---
+++ Movie.cs
@@ -0,0 +1,8 @@
+public class Movie
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public int Duration { get; set; }
+    public string ImageUrl { get; set; }
+}
