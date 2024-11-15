1120b6dc9ad7b584c777cb4be72010c274047772
jaimeescartinsv <a27421@svalero.com>
2024-11-15 14:11:57 +0100

    Update Funcion.cs

--- Funcion.cs
+++ Funcion.cs
@@ -1,8 +1,8 @@
 public class Funcion
 {
     public int FuncionId { get; set; }
-    public int SalaId { get; set; }  // Relación con la sala
-    public int PeliculaId { get; set; }  // Relación con la película
+    public int SalaId { get; set; }
+    public int PeliculaId { get; set; }
     public DateTime FechaDeFuncion { get; set; }
     public DateTime HoraDeInicio { get; set; }
 }
