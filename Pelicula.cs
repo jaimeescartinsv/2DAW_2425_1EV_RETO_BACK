755609bdc3318e53d99696b19ada68522b02d390
jaimeescartinsv <a27421@svalero.com>
2024-11-18 17:46:16 +0100

    añadido campo genero y clasificacion

--- Pelicula.cs
+++ Pelicula.cs
@@ -4,6 +4,8 @@ public class Pelicula
     public string Title { get; set; }
     public string Description { get; set; }
     public string Director { get; set; }
+    public string Genero { get; set; }
+    public string Clasificacion { get; set; }
     public int Duration { get; set; }
     public string ImageUrl { get; set; }
 }
