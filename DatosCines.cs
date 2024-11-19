93537c6f21fe7e005aa9c56f59fcfe1e008e15c1
jaimeescartinsv <a27421@svalero.com>
2024-11-19 21:44:42 +0100

    añadido más sesiones

--- DatosCines.cs
+++ DatosCines.cs
@@ -16,29 +16,12 @@ public static class DatosCines
                     CineId = 1,
                     Sesiones = new List<Sesion>
                     {
-                        new Sesion
-                        {
-                            SesionId = 1,
-                            SalaId = 1,
-                            PeliculaId = 1,
-                            FechaDeSesion = new DateTime(2024, 12, 1),
-                            HoraDeInicio = new DateTime(2024, 12, 1, 18, 0, 0)
-                        },
-                        new Sesion
-                        {
-                            SesionId = 2,
-                            SalaId = 1,
-                            PeliculaId = 2,
-                            FechaDeSesion = new DateTime(2024, 12, 1),
-                            HoraDeInicio = new DateTime(2024, 12, 1, 20, 30, 0)
-                        }
+                        new Sesion { SesionId = 1, SalaId = 1, PeliculaId = 1, FechaDeSesion = new DateTime(2024, 12, 1), HoraDeInicio = new DateTime(2024, 12, 1, 16, 0, 0) },
+                        new Sesion { SesionId = 2, SalaId = 1, PeliculaId = 2, FechaDeSesion = new DateTime(2024, 12, 1), HoraDeInicio = new DateTime(2024, 12, 1, 18, 30, 0) },
+                        new Sesion { SesionId = 3, SalaId = 1, PeliculaId = 3, FechaDeSesion = new DateTime(2024, 12, 1), HoraDeInicio = new DateTime(2024, 12, 1, 21, 0, 0) },
+                        new Sesion { SesionId = 4, SalaId = 1, PeliculaId = 4, FechaDeSesion = new DateTime(2024, 12, 2), HoraDeInicio = new DateTime(2024, 12, 2, 16, 0, 0) }
                     },
-                    Butacas = Enumerable.Range(1, 100).Select(id => new Butaca
-                    {
-                        ButacaId = id,
-                        SalaId = 1,
-                        Estado = "Disponible"
-                    }).ToList()
+                    Butacas = Enumerable.Range(1, 100).Select(id => new Butaca { ButacaId = id, SalaId = 1, Estado = "Disponible" }).ToList()
                 },
                 new Sala
                 {
@@ -47,29 +30,45 @@ public static class DatosCines
                     CineId = 1,
                     Sesiones = new List<Sesion>
                     {
-                        new Sesion
-                        {
-                            SesionId = 3,
-                            SalaId = 2,
-                            PeliculaId = 3,
-                            FechaDeSesion = new DateTime(2024, 12, 2),
-                            HoraDeInicio = new DateTime(2024, 12, 2, 18, 30, 0)
-                        },
-                        new Sesion
-                        {
-                            SesionId = 4,
-                            SalaId = 2,
-                            PeliculaId = 4,
-                            FechaDeSesion = new DateTime(2024, 12, 2),
-                            HoraDeInicio = new DateTime(2024, 12, 2, 21, 0, 0)
-                        }
+                        new Sesion { SesionId = 5, SalaId = 2, PeliculaId = 5, FechaDeSesion = new DateTime(2024, 12, 2), HoraDeInicio = new DateTime(2024, 12, 2, 18, 30, 0) },
+                        new Sesion { SesionId = 6, SalaId = 2, PeliculaId = 6, FechaDeSesion = new DateTime(2024, 12, 2), HoraDeInicio = new DateTime(2024, 12, 2, 21, 0, 0) },
+                        new Sesion { SesionId = 7, SalaId = 2, PeliculaId = 7, FechaDeSesion = new DateTime(2024, 12, 3), HoraDeInicio = new DateTime(2024, 12, 3, 16, 0, 0) },
+                        new Sesion { SesionId = 8, SalaId = 2, PeliculaId = 8, FechaDeSesion = new DateTime(2024, 12, 3), HoraDeInicio = new DateTime(2024, 12, 3, 18, 30, 0) }
                     },
-                    Butacas = Enumerable.Range(1, 100).Select(id => new Butaca
+                    Butacas = Enumerable.Range(1, 100).Select(id => new Butaca { ButacaId = id, SalaId = 2, Estado = "Disponible" }).ToList()
+                },
+                new Sala
+                {
+                    SalaId = 3,
+                    Nombre = "Sala 3",
+                    CineId = 1,
+                    Sesiones = new List<Sesion>
                     {
-                        ButacaId = id,
-                        SalaId = 2,
-                        Estado = "Disponible"
-                    }).ToList()
+                        new Sesion { SesionId = 9, SalaId = 3, PeliculaId = 9, FechaDeSesion = new DateTime(2024, 12, 3), HoraDeInicio = new DateTime(2024, 12, 3, 21, 0, 0) },
+                        new Sesion { SesionId = 10, SalaId = 3, PeliculaId = 10, FechaDeSesion = new DateTime(2024, 12, 4), HoraDeInicio = new DateTime(2024, 12, 4, 16, 0, 0) },
+                        new Sesion { SesionId = 11, SalaId = 3, PeliculaId = 11, FechaDeSesion = new DateTime(2024, 12, 4), HoraDeInicio = new DateTime(2024, 12, 4, 18, 30, 0) },
+                        new Sesion { SesionId = 12, SalaId = 3, PeliculaId = 12, FechaDeSesion = new DateTime(2024, 12, 4), HoraDeInicio = new DateTime(2024, 12, 4, 21, 0, 0) },
+                        new Sesion { SesionId = 13, SalaId = 3, PeliculaId = 13, FechaDeSesion = new DateTime(2024, 12, 5), HoraDeInicio = new DateTime(2024, 12, 5, 16, 0, 0) },
+                        new Sesion { SesionId = 14, SalaId = 3, PeliculaId = 14, FechaDeSesion = new DateTime(2024, 12, 5), HoraDeInicio = new DateTime(2024, 12, 5, 18, 30, 0) }
+                    },
+                    Butacas = Enumerable.Range(1, 100).Select(id => new Butaca { ButacaId = id, SalaId = 3, Estado = "Disponible" }).ToList()
+                },
+                new Sala
+                {
+                    SalaId = 4,
+                    Nombre = "Sala 4",
+                    CineId = 1,
+                    Sesiones = new List<Sesion>
+                    {
+                        new Sesion { SesionId = 15, SalaId = 4, PeliculaId = 15, FechaDeSesion = new DateTime(2024, 12, 5), HoraDeInicio = new DateTime(2024, 12, 5, 21, 0, 0) },
+                        new Sesion { SesionId = 16, SalaId = 4, PeliculaId = 16, FechaDeSesion = new DateTime(2024, 12, 6), HoraDeInicio = new DateTime(2024, 12, 6, 16, 0, 0) },
+                        new Sesion { SesionId = 17, SalaId = 4, PeliculaId = 17, FechaDeSesion = new DateTime(2024, 12, 6), HoraDeInicio = new DateTime(2024, 12, 6, 18, 30, 0) },
+                        new Sesion { SesionId = 18, SalaId = 4, PeliculaId = 18, FechaDeSesion = new DateTime(2024, 12, 6), HoraDeInicio = new DateTime(2024, 12, 6, 21, 0, 0) },
+                        new Sesion { SesionId = 19, SalaId = 4, PeliculaId = 19, FechaDeSesion = new DateTime(2024, 12, 7), HoraDeInicio = new DateTime(2024, 12, 7, 16, 0, 0) },
+                        new Sesion { SesionId = 20, SalaId = 4, PeliculaId = 20, FechaDeSesion = new DateTime(2024, 12, 7), HoraDeInicio = new DateTime(2024, 12, 7, 18, 30, 0) },
+                        new Sesion { SesionId = 21, SalaId = 4, PeliculaId = 21, FechaDeSesion = new DateTime(2024, 12, 7), HoraDeInicio = new DateTime(2024, 12, 7, 21, 0, 0) }
+                    },
+                    Butacas = Enumerable.Range(1, 100).Select(id => new Butaca { ButacaId = id, SalaId = 4, Estado = "Disponible" }).ToList()
                 }
             }
         }
