d11b02dcd57364986d4f4195bd825969fc3688b9
gaabsito <a23185@svalero.com>
2024-11-27 18:49:23 +0100

    añadido precio butaca

--- Butaca.cs
+++ Butaca.cs
@@ -3,6 +3,7 @@ public class Butaca
     public int ButacaId { get; set; }
     public int SalaId { get; set; }
     public string Estado { get; set; } = "Disponible";
+    public double PrecioButaca { get; set; }
     public int? TicketId { get; set; }
 
 }
